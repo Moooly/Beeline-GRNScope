d879e59991fde336ada96e8cf4b0be1260e2da41
Moly Liu <immoooly@gmail.com>
2026-06-18 16:39:52 +0000

    Initial changes

--- Algorithms/ARBORETO/runArboreto.py
+++ Algorithms/ARBORETO/runArboreto.py
@@ -1,10 +1,17 @@
 from optparse import OptionParser
 import os
+from pathlib import Path
 import sys
 import pandas as pd
-from arboreto.algo import grnboost2, genie3
-from arboreto.utils import load_tf_names
-from distributed import Client, LocalCluster
+from arboreto.algo import diy, grnboost2
+from distributed import Client
+
+DEFAULT_GENIE3_TREES = 500
+GENIE3_RF_KWARGS = {
+    'n_jobs': 1,
+    'n_estimators': DEFAULT_GENIE3_TREES,
+    'max_features': 'sqrt',
+}
 
 def parseArgs(args):
     parser = OptionParser()
@@ -18,26 +25,83 @@ def parseArgs(args):
     parser.add_option('', '--outFile', type = 'str',
                       help='File where the output network is stored')
 
+    parser.add_option('', '--nWorkers', type='int', default=None,
+                      help='Number of local Dask workers. Defaults to CPU count.')
+
+    parser.add_option('', '--threadsPerWorker', type='int', default=1,
+                      help='Number of threads per local Dask worker.')
+
+    parser.add_option('', '--genie3Trees', type='int', default=DEFAULT_GENIE3_TREES,
+                      help='Number of random forest trees to use for GENIE3.')
+
+    parser.add_option('', '--runRoot', type='str', default=None,
+                      help='Batch mode root containing run_id/algorithm/working_dir folders.')
+
+    parser.add_option('', '--runIds', type='str', default=None,
+                      help='Comma-separated run IDs to execute in batch mode.')
+
+    parser.add_option('', '--algorithmId', type='str', default=None,
+                      help='Algorithm output directory name used in batch mode.')
+
     (opts, args) = parser.parse_args(args)
 
     return opts, args
 
+def run_inference(algo, in_file, out_file, client, genie3_trees):
+    inDF = pd.read_csv(in_file, sep = '\t', index_col = 0, header = 0)
+    print(f"{algo} loaded expression shape {inDF.shape} from {in_file}", flush=True)
+
+    normalized_algo = str(algo).upper()
+    if normalized_algo == 'GENIE3':
+        rf_kwargs = dict(GENIE3_RF_KWARGS)
+        rf_kwargs['n_estimators'] = genie3_trees
+        print(f"GENIE3 using n_estimators={rf_kwargs['n_estimators']}", flush=True)
+        network = diy(
+            expression_data=inDF.to_numpy(),
+            regressor_type='RF',
+            regressor_kwargs=rf_kwargs,
+            client_or_address=client,
+            gene_names=inDF.columns,
+        )
+    elif normalized_algo == 'GRNBOOST2':
+        network = grnboost2(inDF.to_numpy(), client_or_address = client, gene_names = inDF.columns)
+    else:
+        raise ValueError("Wrong algorithm name. Should either be GENIE3 or GRNBoost2.")
+
+    network.to_csv(out_file, index = False, sep = '\t')
+
 def main(args):
     opts, args = parseArgs(args)
-    inDF = pd.read_csv(opts.inFile, sep = '\t', index_col = 0, header = 0)
 
-    client = Client(processes = False)    
+    n_workers = opts.nWorkers or os.cpu_count() or 1
+    client = Client(
+        processes=False,
+        n_workers=n_workers,
+        threads_per_worker=opts.threadsPerWorker,
+    )
 
-    if opts.algo == 'GENIE3':
-        network = genie3(inDF.to_numpy(), client_or_address = client, gene_names = inDF.columns)
-        network.to_csv(opts.outFile, index = False, sep = '\t')
+    try:
+        if opts.runRoot:
+            if not opts.runIds or not opts.algorithmId:
+                raise ValueError("--runRoot batch mode requires --runIds and --algorithmId.")
 
-    elif opts.algo == 'GRNBoost2':
-        network = grnboost2(inDF.to_numpy(), client_or_address = client, gene_names = inDF.columns)
-        network.to_csv(opts.outFile, index = False, sep = '\t')
-
-    else:
-        print("Wrong algorithm name. Should either be GENIE3 or GRNBoost2.")
+            run_root = Path(opts.runRoot)
+            run_ids = [run_id.strip() for run_id in opts.runIds.split(',') if run_id.strip()]
+            for run_id in run_ids:
+                working_dir = run_root / run_id / opts.algorithmId / 'working_dir'
+                run_inference(
+                    opts.algo,
+                    working_dir / 'ExpressionData.csv',
+                    working_dir / 'outFile.txt',
+                    client,
+                    opts.genie3Trees,
+                )
+        else:
+            if not opts.inFile or not opts.outFile:
+                raise ValueError("Single-run mode requires --inFile and --outFile.")
+            run_inference(opts.algo, opts.inFile, opts.outFile, client, opts.genie3Trees)
+    finally:
+        client.close()
                         
 if __name__ == "__main__":
     main(sys.argv)
--- BLRun/genie3Runner.py
+++ BLRun/genie3Runner.py
@@ -1,7 +1,28 @@
 import os
 import pandas as pd
+from pathlib import Path
 
 from BLRun.runner import Runner
+
+ARBORETO_SCRIPT = Path(__file__).resolve().parent.parent / "Algorithms" / "ARBORETO" / "runArboreto.py"
+
+
+def arboreto_dask_args():
+    worker_count = os.environ.get("GRNSCOPE_ARBORETO_DASK_WORKERS")
+    if not worker_count:
+        try:
+            concurrent_tasks = max(1, int(os.environ.get("GRNSCOPE_MAX_CONCURRENT_ALGORITHMS", "2")))
+        except ValueError:
+            concurrent_tasks = 2
+        worker_count = str(max(1, (os.cpu_count() or 1) // concurrent_tasks))
+
+    threads_per_worker = os.environ.get("GRNSCOPE_ARBORETO_THREADS_PER_WORKER", "1")
+    return [f"--nWorkers={worker_count}", f"--threadsPerWorker={threads_per_worker}"]
+
+
+def genie3_tree_args():
+    tree_count = os.environ.get("GRNSCOPE_GENIE3_TREES", "500")
+    return [f"--genie3Trees={tree_count}"]
 
 
 class GENIE3Runner(Runner):
@@ -32,13 +53,45 @@ class GENIE3Runner(Runner):
 
         cmdToRun = ' '.join(['docker run --rm',
                             f"-v {self.working_dir}:/usr/working_dir",
+                            f"-v {ARBORETO_SCRIPT}:/runArboreto.py:ro",
                             '--expose=41269',
                             f'{self.image} /bin/sh -c \"time -v -o',
                             "/usr/working_dir/time.txt",
-                            'python runArboreto.py --algo=GENIE3',
-                            '--inFile=/usr/working_dir/ExpressionData.csv', '--outFile=/usr/working_dir/outFile.txt', '\"'])
+                            'python /runArboreto.py --algo=GENIE3',
+                            '--inFile=/usr/working_dir/ExpressionData.csv',
+                            '--outFile=/usr/working_dir/outFile.txt',
+                            *arboreto_dask_args(),
+                            *genie3_tree_args(), '\"'])
 
         self._run_docker(cmdToRun)
+
+    @classmethod
+    def run_batch(cls, runners):
+        '''
+        Run multiple GENIE3 confidence runs in one Docker/Dask session.
+        '''
+
+        if not runners:
+            return
+
+        first_runner = runners[0]
+        output_root = first_runner.output_dir.parent.parent
+        run_ids = ','.join(runner.output_dir.parent.name for runner in runners)
+
+        cmdToRun = ' '.join(['docker run --rm',
+                            f"-v {output_root}:/usr/arboreto_runs",
+                            f"-v {ARBORETO_SCRIPT}:/runArboreto.py:ro",
+                            '--expose=41269',
+                            f'{first_runner.image} /bin/sh -c \"time -v -o',
+                            "/usr/arboreto_runs/arboreto_batch_time.txt",
+                            'python /runArboreto.py --algo=GENIE3',
+                            '--runRoot=/usr/arboreto_runs',
+                            f'--runIds={run_ids}',
+                            '--algorithmId=GENIE3',
+                            *arboreto_dask_args(),
+                            *genie3_tree_args(), '\"'])
+
+        first_runner._run_docker(cmdToRun)
 
     def parseOutput(self):
         '''
